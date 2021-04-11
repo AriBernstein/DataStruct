d1606af7653e05468488df4a11df5fd3150aa830
Ari Bernstein <aribernstein1997@gmail.com>
2021-04-11 19:22:20 -0400

    Replaced integer values for data with nodes

--- DataStruct/Fractional Cascading/Nodes/CoordinateNode.cs
+++ DataStruct/Fractional Cascading/Nodes/CoordinateNode.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 namespace Fractional_Cascading {
     public class CoordNode : Node {
-        private int data; // attr code 0
+        private DataNode data; // attr code 0
 
         // attr codes 1, 2, 3
         private int xLoc = -1, yLoc = -1, zLoc = -1;
@@ -11,28 +11,28 @@ namespace Fractional_Cascading {
             xLoc = xVal;
             yLoc = yVal;
             zLoc = zVal;
-            data = dataVal;
+            data = new DataNode(dataVal);
         }
 
         public CoordNode(int dataVal, int xVal, int yVal) {
             // 2-dimensional constructor
             xLoc = xVal;
             yLoc = yVal;
-            data = dataVal;
+            data = new DataNode(dataVal);
         }
 
         public CoordNode(int dataVal, int xVal) {
             // 1-dimenstional constructor
             xLoc = xVal;
-            data = dataVal;
+            data = new DataNode(dataVal);
         }
 
         public void setData(int newData) {
-            data = newData;
+            data = new DataNode(newData);
         }
 
         public int getAttr(int attrCode) {
-            if (attrCode == 0)      return data;
+            if (attrCode == 0)      return data.getAttr(attrCode);
             else if (attrCode == 1) return xLoc;
             else if (attrCode == 2) return yLoc;
             else if (attrCode == 3) return zLoc;
---
+++ DataStruct/Fractional Cascading/Nodes/DataNode.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Fractional_Cascading {
+    public class DataNode : Node {
+        private int data; // attr code 0
+
+        public int getAttr(int attrCode) {
+            if (attrCode == 0) return data;
+            else throw new Exception("getAttr != 0 when called on DataNode");
+        }
+        
+        public DataNode(int dataVal) {
+            data = dataVal;
+        }
+
+        public override string ToString() {
+            return data.ToString();
+        }
+    }
+}
